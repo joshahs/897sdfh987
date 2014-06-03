6e2c763aa35ed39d7f9632fb6d4391901e5c8c8c
ilshat-fatkhullin <ilshat-fatkhullin@yandex.ru>
2014-06-03 19:06:09 +0400

    Office Table

---
+++ Office Table/Office Table.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Office_Table
+{
+    partial class Office_Table
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Office_Table
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 304);
+            this.Name = "Office_Table";
+            this.Text = "Office_Table";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Office Table/Office Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Office_Table
+{
+    public partial class Office_Table : Form
+    {
+        public Office_Table()
+        {
+            InitializeComponent();
+        }
+    }
+}
